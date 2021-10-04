d0bfb1600d05b78fddc9e037b9d7f27af4657165
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-10-04 10:31:50 +0800

    Update binsrch1.cpp

--- HW class/c1/binsrch1.cpp
+++ HW class/c1/binsrch1.cpp
@@ -1,4 +1,6 @@
-#include <iostream.h>
+#include <iostream>
+
+using namespace std;
 
 char compare (int x, int y)
 {
@@ -21,7 +23,7 @@ int BinarySearch (int *a, int x, const int n)
  return -1; // not found
 } // end of BinarySearch
 
-void main()
+int main()
 {
    int b[10];
    b[0] = 2; b[1] = 4; b[2] = 5; b[3] = 7; b[4] = 10;
