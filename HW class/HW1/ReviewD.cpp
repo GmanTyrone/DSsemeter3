7f318f811bf4422d55b58c637ada0b9b3926a49b
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-10-12 16:01:31 +0800

    Create ReviewD.cpp

---
+++ HW class/HW1/ReviewD.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int calculate(char operador, int a, int b)
+{
+    try{
+        if(operador!='*'&&operador!='/'&&operador!='-'&&operador!='+')
+            throw operador;
+        }
+        catch(char x){
+            cout<<"Invalid operator "<<x<<endl;
+            return NULL;
+        }
+    switch(operador){
+    case '*':
+        return a*b;
+    case '+':
+        return a+b;
+    case '-':
+        return a-b;
+    case '/':
+        try{
+            if(b==0)throw &operador;
+            return a/b;
+        }catch(char* operador){
+            cout<<"Division by 0"<<endl;
+            return NULL;
+        }
+    }
+}
+
+
+int main(void)
+{
+    int x,y,z;
+    char op;
+    while(1)
+    {
+        cin>>x>>op>>y;
+        z=calculate(op,x,y);
+        cout<<z<<endl;
+    }
+    return 0;
+}
