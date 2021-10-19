d6b351ec2e5b77e2ff7348d978f6985ce6bd33e9
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-10-19 19:11:57 +0800

    x

--- HW class/HW1/ReviewC.cpp
+++ HW class/HW1/ReviewC.cpp
@@ -5,27 +5,38 @@
 
 using namespace std;
 
-int alfabeto[26]={0};
+
 
 int main()
 {
-    string filename="Ode to a Nightingale", fileoutput="histograma";
+    string sentence;
     ifstream poem;
     ofstream chart;
 
+    int alfabeto[26]={0};
+
     //Counting the letters
-    poem.open(filename);
-    while(!(poem.eof())){
-        int c=getchar();
-        if(c>64&&c<91)c-=65;
-        else if(c>96&&c<123)c-=97;
-        else  c+=100;
-        if(c<26)alfabeto[c]++;
+    poem.open("Poema.txt",ios::in);
+
+
+    while(getline(poem,sentence)){
+        for(int k=0;k<sentence.size();k++){
+
+            int c=getchar();
+
+
+            //FIRST IFS
+            if(c>64&&c<91)c-=65;
+            else if(c>96&&c<123)c-=97;
+            else  c+=26;
+
+            //THEN WE DECIDE WHAT TO DO WITH THE IF
+            if(c<26)alfabeto[c]++;
+        }
     }
 
-
     //Printing it
-    chart.open(fileoutput);
+    chart.open("histograma.txt");
     for(int i=0;i<=25;i++){
         if(alfabeto[i]!=0){
             chart<<(char)(i+97);
