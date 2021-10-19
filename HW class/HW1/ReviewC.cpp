85c23b69067a6d2056f6c46b55c26f4ccacf7197
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-10-19 18:31:23 +0800

    Update ReviewC.cpp

--- HW class/HW1/ReviewC.cpp
+++ HW class/HW1/ReviewC.cpp
@@ -1,17 +1,41 @@
-#include <cstdlib>
 #include <fstream>
-#include <iostream>
 #include <string>
+#include <cstdio>
+
 
 using namespace std;
 
-
+int alfabeto[26]={0};
 
 int main()
 {
-    string filename="Ode to a Nightingale";
-    ifstream file;
-    file.open(filename);
-    while()
+    string filename="Ode to a Nightingale", fileoutput="histograma";
+    ifstream poem;
+    ofstream chart;
+
+    //Counting the letters
+    poem.open(filename);
+    while(!(poem.eof())){
+        int c=getchar();
+        if(c>64&&c<91)c-=65;
+        else if(c>96&&c<123)c-=97;
+        else  c+=100;
+        if(c<26)alfabeto[c]++;
+    }
+
+
+    //Printing it
+    chart.open(fileoutput);
+    for(int i=0;i<=25;i++){
+        if(alfabeto[i]!=0){
+            chart<<(char)(i+97);
+            for(int j=0;j<=alfabeto[i];j++){
+                chart<<"*";
+            }
+            chart<<alfabeto[i]<<endl;
+        }
+    }
+    chart.close();
     return 0;
 }
+
