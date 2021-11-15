18048c46756dc931e9efc06043f1a9415169d485
GmanTyrone <50692892+GmanTyrone@users.noreply.github.com>
2021-11-16 03:09:42 +0800

    Update Triple Node.cpp

--- HW class/Exam/Triple Node.cpp
+++ HW class/Exam/Triple Node.cpp
@@ -1,184 +1,107 @@
-#include<iostream>
-#include<conio.h>
-#include<stdlib.h>
+#include <iostream>
+#include <string>
+#define MAXZISE 100
 
 using namespace std;
 
-class node
+class Node
 {
 public:
-    int data;
-    class node *next;
-    class node *prev;
-
+	int data;
+	Node *next=nullptr;
+	Node *prev=nullptr;
 };
 
-class dqueue : public node
-
+class Queue
 {
-    node *head, *tail,*mid;
-    int top1, top2,middle;
+public:
+	Queue()
+	{
+        top=new Node;
+        bot=top;
+        mid=top;
+        count=0;
+	}
 
-    public:
+	//TOP
+	int enqueueTOP(int d)
+	{
+	    if(count=0){
+            top->data=d;
+        }
+        else{
+            Node *temp=top;
+            top=new Node;
+            top->data=d;
+            temp->next=top;
+            top->prev=temp;
+        }
+        count++;
+        return 1;
+	}
+	int *dequeueTOP()
+	{
+	    if(count!=0){
+            Node *temp=top;
+            top=top->prev;
+            count--;
+            if(count%2==0)mid=mid->prev;
+            return &(temp->data);
+        }
+        else return nullptr;
+	}
 
-    dqueue()
-    {
-        top1 = 0;
-        top2 = 0;
-        mid = 0;
-        head = NULL;
-        tail = NULL;
-    }
+	//Middle
+	int *dequeueMID()
+	{
+	    if(count!=0&&count%2!=0){
+            Node *temp=mid;
+            mid->next->prev=mid->prev;
+            mid->prev->next=mid->next;
+            count--;
+            return &(temp->data);
+        }
+        else return nullptr;
+	}
 
-    void push(int x) {
-        node *temp;
-        int ch;
-        if (top1 + top2 >= 5)
-        {
-            cout << "dqueue overflow";
-            return;
-        }
-        if (top1 + top2 == 0)
-        {
-            head = new node;
-            head->data = x;
-            head->next = NULL;
-            head->prev = NULL;
-            tail = head;
-            top1++;
-        }
-        else
-        {
-            cout << " Add element 1.FIRST 2.Middle 3.LAST\n enter ur choice:";
-            cin >> ch;
-            if (ch == 1)
-            {
-                top1++;
-                temp = new node;
-                temp->data = x;
-                temp->next = head;
-                temp->prev = NULL;
-                head->prev = temp;
-                head = temp;
-            }
-            else if(ch==2)
-            {
-                middle = top1 + top2;
-                temp = new node;
-                mid->next = temp;
-                temp->next = mid->next;
-                temp->prev = temp;
-            }
-            else
-            {
-                top2++;
-                temp = new node;
-                temp->data = x;
-                temp->next = NULL;
-                temp->prev = tail;
-                tail->next = temp;
-                tail = temp;
-            }
-        }
 
-    }
+	//BOT
+	int enqueueBOT(int d)
+	{
+	    if(count=0){
+            top->data=d;
+        }
+        else{
+            Node *temp=bot;
+            bot=new Node;
+            bot->data=d;
+            temp->prev=bot;
+            bot->next=temp;
+        }
+        count++;
+        return 1;
+	}
+	int *dequeueBOT()
+	{
+	    if(count!=0){
+            Node *temp=bot;
+            bot=bot->next;
+            count--;
+            if(count%2==0)mid=mid->next;
+            return &(temp->data);
+        }
+        else return nullptr;
+	}
 
-    void pop()
-    {
-        int ch;
-        cout << "Delete 1.First Node 2.Last Node 3.Middle Node\n Enter ur choice:";
-        cin >> ch;
-        if (top1 + top2 <= 0)
-        {
-            cout << "\nDqueue under flow";
-            return;
-        }
-        if (ch == 1)
-        {
-            head = head->next;
-            head->prev = NULL;
-            top1--;
-        }
-        else if(ch==2)
-        {
-            top2--;
-            tail = tail->prev;
-            tail->next = NULL;
-        }
-        else
-        {
-            middle=top1+top2;
-            mid->next = mid->next->next;
-            mid->prev = mid->prev->prev;
-        }
-    }
 
-    void display()
-    {
-        int ch;
-        node *temp;
-        cout << "Display from 1.Staring 2.Ending\n Enter ur choice";
-        cin >> ch;
-        if (top1 + top2 <= 0)
-        {
-            cout << "under flow";
-            return;
-        }
-        if (ch == 1)
-        {
-            temp = head;
-            while (temp != NULL)
-            {
-                cout << temp->data << " \n";
-                temp = temp->next;
-            }
-        }
-        else
-        {
-            temp = tail;
-            while (temp != NULL)
-            {
-                cout << temp->data << " \n";
-                temp = temp->prev;
-            }
-        }
-    }
+private:
+	Node *top;
+    Node *bot;
+    Node *mid;
+    int count;
 };
 
-int main()
-
+int main(void)
 {
-
-dqueue d1;
-
-int ch;
-
-while (1) {
-
-cout << "1.INSERT 2.DELETE 3.DISPLAY 4.EXIT\n Enter ur choice:";
-
-cin >> ch;
-
-switch (ch)
-
-{
-
-case 1: cout << "Enter element";
-
-cin >> ch;
-
-d1.push(ch); break;
-
-case 2: d1.pop(); break;
-
-case 3: d1.display(); break;
-
-case 4: exit(1);
-
-}
-
-}
-
-return 0;
 
 }
