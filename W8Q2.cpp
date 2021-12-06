06570f30b1e6a103af603259354a10b17c41395b
GmanTyrone <50692892+GmanTyrone@users.noreply.github.com>
2021-12-06 09:43:21 +0800

    z

--- W8Q2.cpp
+++ W8Q2.cpp
@@ -76,28 +76,45 @@ public:
 	}
 	void levelorder()
 	{
+	    queue<int> q;
+	    int x=0;
+	    while(root[x][0]!=-1){
+            cout<<root[x][1]<<" ";
+            if(root[x][2]!=-1)q.push(root[x][2]);
+            if(root[x][3]!=-1)q.push(root[x][3]);
+            if(q.empty())return;
+            x=q.front();
+            q.pop();
+	    }
 	}
 private:
     void inorder(int x)
     {
-       if(root[x][2]!=-1){
-			inorder(root[x][2]);
+		if(root[x][0]!=-1){
+			if(root[x][2]!=-1)inorder(root[x][2]);
+            cout<<root[x][1]<<" ";
+            if(root[x][3]!=-1)inorder(root[x][3]);
 		}
-		cout<<root[x][1]<<" ";
-		if(root[x][3]!=-1){
-			inorder(root[x][3]);
-		}
-		if(root[x][2] == -1 && root[x][3] == -1){
-			return;
-		}
+		return;
+
     }
     void preorder(int x)
     {
-
+        if(root[x][0]!=-1){
+			cout<<root[x][1]<<" ";
+			if(root[x][2]!=-1)preorder(root[x][2]);
+            if(root[x][3]!=-1)preorder(root[x][3]);
+		}
+		return;
     }
     void postorder(int x)
     {
-
+        if(root[x][0]!=-1){
+			if(root[x][2]!=-1)postorder(root[x][2]);
+            if(root[x][3]!=-1)postorder(root[x][3]);
+            cout<<root[x][1]<<" ";
+		}
+		return;
     }
 
 	int root[20][4];
@@ -110,7 +127,7 @@ int main()
 	srand(time(NULL));
 	for(j = 0;j < 10;j ++)
 	{
-		node = j;//rand();
+		node = rand();
 		tree->insert(node);
 	}
 	tree->inorder();
